d54700aed8dc315bbd83cc52186a984087b4c4c7
nelsk <nelsk@CARIAS2021>
2022-11-04 15:40:20 -0500

    giusab na pud

--- frmPayment.Designer.cs
+++ frmPayment.Designer.cs
@@ -31,13 +31,12 @@ namespace SamecProject
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPayment));
             this.grpMember = new System.Windows.Forms.GroupBox();
-            this.txtMemberInductiondate = new System.Windows.Forms.MaskedTextBox();
             this.cmbYears = new System.Windows.Forms.ComboBox();
             this.cmbMonths = new System.Windows.Forms.ComboBox();
             this.cmbPaymentType = new System.Windows.Forms.ComboBox();
             this.label11 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.txtMemberEmail = new System.Windows.Forms.TextBox();
+            this.txtPaymentAmount = new System.Windows.Forms.TextBox();
             this.txtMemberFirstname = new System.Windows.Forms.TextBox();
             this.txtMemberLastName = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
@@ -49,18 +48,21 @@ namespace SamecProject
             this.label1 = new System.Windows.Forms.Label();
             this.txtMemberID = new System.Windows.Forms.TextBox();
             this.btnMemberSave = new System.Windows.Forms.Button();
+            this.dtpPaymentDate = new System.Windows.Forms.DateTimePicker();
+            this.txtPaymentID = new System.Windows.Forms.TextBox();
             this.grpMember.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpMember
             // 
-            this.grpMember.Controls.Add(this.txtMemberInductiondate);
+            this.grpMember.Controls.Add(this.txtPaymentID);
+            this.grpMember.Controls.Add(this.dtpPaymentDate);
             this.grpMember.Controls.Add(this.cmbYears);
             this.grpMember.Controls.Add(this.cmbMonths);
             this.grpMember.Controls.Add(this.cmbPaymentType);
             this.grpMember.Controls.Add(this.label11);
             this.grpMember.Controls.Add(this.label9);
-            this.grpMember.Controls.Add(this.txtMemberEmail);
+            this.grpMember.Controls.Add(this.txtPaymentAmount);
             this.grpMember.Controls.Add(this.txtMemberFirstname);
             this.grpMember.Controls.Add(this.txtMemberLastName);
             this.grpMember.Controls.Add(this.label7);
@@ -79,28 +81,15 @@ namespace SamecProject
             this.grpMember.TabIndex = 1;
             this.grpMember.TabStop = false;
             // 
-            // txtMemberInductiondate
-            // 
-            this.txtMemberInductiondate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtMemberInductiondate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMemberInductiondate.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.txtMemberInductiondate.Location = new System.Drawing.Point(135, 226);
-            this.txtMemberInductiondate.Mask = "00/00/0000";
-            this.txtMemberInductiondate.Name = "txtMemberInductiondate";
-            this.txtMemberInductiondate.Size = new System.Drawing.Size(205, 27);
-            this.txtMemberInductiondate.TabIndex = 28;
-            this.txtMemberInductiondate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.txtMemberInductiondate.ValidatingType = typeof(System.DateTime);
-            // 
             // cmbYears
             // 
             this.cmbYears.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.cmbYears.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbYears.FormattingEnabled = true;
-            this.cmbYears.Location = new System.Drawing.Point(135, 195);
+            this.cmbYears.Location = new System.Drawing.Point(135, 194);
             this.cmbYears.Name = "cmbYears";
             this.cmbYears.Size = new System.Drawing.Size(205, 25);
-            this.cmbYears.TabIndex = 27;
+            this.cmbYears.TabIndex = 14;
             this.cmbYears.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMemberID_KeyPress);
             // 
             // cmbMonths
@@ -109,10 +98,10 @@ namespace SamecProject
             this.cmbMonths.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.cmbMonths.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbMonths.FormattingEnabled = true;
-            this.cmbMonths.Location = new System.Drawing.Point(135, 165);
+            this.cmbMonths.Location = new System.Drawing.Point(135, 164);
             this.cmbMonths.Name = "cmbMonths";
             this.cmbMonths.Size = new System.Drawing.Size(205, 25);
-            this.cmbMonths.TabIndex = 26;
+            this.cmbMonths.TabIndex = 13;
             // 
             // cmbPaymentType
             // 
@@ -120,17 +109,17 @@ namespace SamecProject
             this.cmbPaymentType.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.cmbPaymentType.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbPaymentType.FormattingEnabled = true;
-            this.cmbPaymentType.Location = new System.Drawing.Point(135, 135);
+            this.cmbPaymentType.Location = new System.Drawing.Point(135, 134);
             this.cmbPaymentType.Name = "cmbPaymentType";
             this.cmbPaymentType.Size = new System.Drawing.Size(205, 25);
-            this.cmbPaymentType.TabIndex = 25;
+            this.cmbPaymentType.TabIndex = 12;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(41, 44);
+            this.label11.Location = new System.Drawing.Point(41, 43);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(91, 16);
             this.label11.TabIndex = 22;
@@ -141,23 +130,23 @@ namespace SamecProject
             this.label9.BackColor = System.Drawing.Color.White;
             this.label9.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Red;
-            this.label9.Location = new System.Drawing.Point(138, 44);
+            this.label9.Location = new System.Drawing.Point(318, 44);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(18, 16);
             this.label9.TabIndex = 18;
             this.label9.Text = "*";
             // 
-            // txtMemberEmail
+            // txtPaymentAmount
             // 
-            this.txtMemberEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtMemberEmail.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMemberEmail.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.txtMemberEmail.Location = new System.Drawing.Point(135, 257);
-            this.txtMemberEmail.MaxLength = 50;
-            this.txtMemberEmail.Name = "txtMemberEmail";
-            this.txtMemberEmail.Size = new System.Drawing.Size(205, 27);
-            this.txtMemberEmail.TabIndex = 16;
-            this.txtMemberEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPaymentAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPaymentAmount.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaymentAmount.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtPaymentAmount.Location = new System.Drawing.Point(135, 256);
+            this.txtPaymentAmount.MaxLength = 50;
+            this.txtPaymentAmount.Name = "txtPaymentAmount";
+            this.txtPaymentAmount.Size = new System.Drawing.Size(205, 27);
+            this.txtPaymentAmount.TabIndex = 16;
+            this.txtPaymentAmount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPaymentAmount_KeyPress);
             // 
             // txtMemberFirstname
             // 
@@ -171,7 +160,6 @@ namespace SamecProject
             this.txtMemberFirstname.Name = "txtMemberFirstname";
             this.txtMemberFirstname.Size = new System.Drawing.Size(205, 27);
             this.txtMemberFirstname.TabIndex = 11;
-            this.txtMemberFirstname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtMemberLastName
             // 
@@ -180,19 +168,18 @@ namespace SamecProject
             this.txtMemberLastName.Enabled = false;
             this.txtMemberLastName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtMemberLastName.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.txtMemberLastName.Location = new System.Drawing.Point(135, 71);
+            this.txtMemberLastName.Location = new System.Drawing.Point(135, 70);
             this.txtMemberLastName.MaxLength = 20;
             this.txtMemberLastName.Name = "txtMemberLastName";
             this.txtMemberLastName.Size = new System.Drawing.Size(205, 27);
             this.txtMemberLastName.TabIndex = 10;
-            this.txtMemberLastName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(62, 263);
+            this.label7.Location = new System.Drawing.Point(62, 260);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(70, 16);
             this.label7.TabIndex = 8;
@@ -203,7 +190,7 @@ namespace SamecProject
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(22, 232);
+            this.label6.Location = new System.Drawing.Point(22, 229);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(110, 16);
             this.label6.TabIndex = 7;
@@ -214,7 +201,7 @@ namespace SamecProject
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(82, 201);
+            this.label5.Location = new System.Drawing.Point(82, 198);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(50, 16);
             this.label5.TabIndex = 6;
@@ -225,7 +212,7 @@ namespace SamecProject
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(72, 170);
+            this.label4.Location = new System.Drawing.Point(72, 167);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 16);
             this.label4.TabIndex = 5;
@@ -236,7 +223,7 @@ namespace SamecProject
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(21, 139);
+            this.label3.Location = new System.Drawing.Point(21, 136);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(111, 16);
             this.label3.TabIndex = 4;
@@ -247,7 +234,7 @@ namespace SamecProject
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(50, 108);
+            this.label2.Location = new System.Drawing.Point(50, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 16);
             this.label2.TabIndex = 3;
@@ -258,7 +245,7 @@ namespace SamecProject
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(50, 77);
+            this.label1.Location = new System.Drawing.Point(50, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(82, 16);
             this.label1.TabIndex = 2;
@@ -274,7 +261,7 @@ namespace SamecProject
             this.txtMemberID.Name = "txtMemberID";
             this.txtMemberID.Size = new System.Drawing.Size(205, 27);
             this.txtMemberID.TabIndex = 9;
-            this.txtMemberID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtMemberID.TextChanged += new System.EventHandler(this.txtMemberID_TextChanged);
             this.txtMemberID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMemberID_KeyPress);
             // 
             // btnMemberSave
@@ -286,9 +273,33 @@ namespace SamecProject
             this.btnMemberSave.Location = new System.Drawing.Point(158, 334);
             this.btnMemberSave.Name = "btnMemberSave";
             this.btnMemberSave.Size = new System.Drawing.Size(110, 28);
-            this.btnMemberSave.TabIndex = 19;
+            this.btnMemberSave.TabIndex = 17;
             this.btnMemberSave.Text = "Save";
             this.btnMemberSave.UseVisualStyleBackColor = false;
+            this.btnMemberSave.Click += new System.EventHandler(this.btnMemberSave_Click);
+            // 
+            // dtpPaymentDate
+            // 
+            this.dtpPaymentDate.CalendarFont = new System.Drawing.Font("Century", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpPaymentDate.CustomFormat = "MM/dd/yyyy";
+            this.dtpPaymentDate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpPaymentDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpPaymentDate.Location = new System.Drawing.Point(135, 224);
+            this.dtpPaymentDate.Name = "dtpPaymentDate";
+            this.dtpPaymentDate.Size = new System.Drawing.Size(205, 27);
+            this.dtpPaymentDate.TabIndex = 24;
+            // 
+            // txtPaymentID
+            // 
+            this.txtPaymentID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPaymentID.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaymentID.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtPaymentID.Location = new System.Drawing.Point(135, 14);
+            this.txtPaymentID.MaxLength = 8;
+            this.txtPaymentID.Name = "txtPaymentID";
+            this.txtPaymentID.Size = new System.Drawing.Size(205, 27);
+            this.txtPaymentID.TabIndex = 25;
+            this.txtPaymentID.Visible = false;
             // 
             // frmPayment
             // 
@@ -299,6 +310,8 @@ namespace SamecProject
             this.Controls.Add(this.btnMemberSave);
             this.Controls.Add(this.grpMember);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmPayment";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Payment";
@@ -314,7 +327,7 @@ namespace SamecProject
         private System.Windows.Forms.GroupBox grpMember;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox txtMemberEmail;
+        private System.Windows.Forms.TextBox txtPaymentAmount;
         private System.Windows.Forms.TextBox txtMemberFirstname;
         private System.Windows.Forms.TextBox txtMemberLastName;
         private System.Windows.Forms.Label label7;
@@ -329,6 +342,7 @@ namespace SamecProject
         private System.Windows.Forms.ComboBox cmbPaymentType;
         private System.Windows.Forms.ComboBox cmbYears;
         private System.Windows.Forms.ComboBox cmbMonths;
-        private System.Windows.Forms.MaskedTextBox txtMemberInductiondate;
+        private System.Windows.Forms.DateTimePicker dtpPaymentDate;
+        private System.Windows.Forms.TextBox txtPaymentID;
     }
 }
--- frmPayment.cs
+++ frmPayment.cs
@@ -60,8 +60,96 @@ namespace SamecProject
                 
         private void txtMemberID_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                 if (!string.IsNullOrEmpty(txtMemberID.Text))
+                {
+                    GetMember(txtMemberID.Text);
+                }
+            } else
+            {
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            }
+            
         }
-        
+
+        private void GetMember(string memid)
+        {
+            using (SqlConnection conn = new SqlConnection(GetSetClass.sqlconnectstring))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("GetMember", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@MemberID", memid);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    conn.Open();
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "tblMember");
+                    conn.Close();
+                    txtMemberFirstname.Text = ds.Tables["tblMember"].Rows[0]["Firstname"].ToString();
+                    txtMemberLastName.Text = ds.Tables["tblMember"].Rows[0]["Lastname"].ToString();
+                    cmbPaymentType.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+
+        }
+        private void btnMemberSave_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtMemberID.Text) && !string.IsNullOrEmpty(cmbPaymentType.Text) && !string.IsNullOrEmpty(cmbMonths.Text) && !string.IsNullOrEmpty(cmbYears.Text) 
+                && !string.IsNullOrEmpty(txtPaymentAmount.Text))
+            {
+                AddUpdatePayment("add");
+            }
+        }
+
+        private void AddUpdatePayment(string tt)
+        {
+            using (SqlConnection conn = new SqlConnection(GetSetClass.sqlconnectstring))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("AddUpdatePayment", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@MemberID", memid);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    conn.Open();
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "tblMember");
+                    conn.Close();
+                    txtMemberFirstname.Text = ds.Tables["tblMember"].Rows[0]["Firstname"].ToString();
+                    txtMemberLastName.Text = ds.Tables["tblMember"].Rows[0]["Lastname"].ToString();
+                    cmbPaymentType.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+
+        }
+
+        private void txtMemberID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPaymentAmount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+            
+            if (e.KeyChar == '.'
+                && (sender as TextBox).Text.IndexOf('.') > -1)
+            {
+                e.Handled = true;
+            }
+        }
     }
 }
